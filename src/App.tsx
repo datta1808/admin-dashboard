be6cf64d4a8fd25869169c24215b53003c1711ea
dattatreya.bagale <dattatreya.bagale@blazeclan.com>
2022-11-09 17:46:50 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import  Dashboard  from './Components/Dashboard/Dashboard';
+import UserDashboard from './Components/UserDashboard/UserDashborad';
+import {Login} from './Components/Login/Login'
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+    return (
+        <BrowserRouter>
+            <Routes>
+            <Route path="/" element={<Login/>}/>
+             <Route path="/dashboard" element={<Dashboard/>}/>
+             <Route path="/user" element={<UserDashboard/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
